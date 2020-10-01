55f0c8250902e6730b04af48bc330ea0b14d1f3c
Vipin Mishra <thevipinmishra@gmail.com>
2020-10-01 07:41:55 -0700

    Removed Jekyll Instance

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector("nav");
-const menu = document.querySelector(".menu");
-
-hamburger.addEventListener("click", handleMenu);
-
-function handleMenu() {
-  nav.classList.toggle("menu__expanded");
-}
