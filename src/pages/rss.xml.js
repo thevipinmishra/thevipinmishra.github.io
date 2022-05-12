ddb59168cd6ccafa5aa3d962e112bbf9d92f25c0
Vipin Mishra <vipin@digiqt.com>
2022-05-12 12:34:00 +0530

    updated rss integration

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,9 @@
 // src/pages/rss.xml.js
 import rss from "@astrojs/rss";
 
+const postImportResult = import.meta.globEager("./blog/*.md");
+const posts = Object.values(postImportResult);
+
 export const get = () =>
   rss({
     // `<title>` field in output xml
@@ -14,7 +17,11 @@ export const get = () =>
     // list of `<item>`s in output xml
     // simple example: generate items for every md file in /src/pages
     // see "Generating items" section for required frontmatter and advanced use cases
-    items: import.meta.glob("./**/*.md"),
+    items: posts.map((post) => ({
+      link: post.frontmatter.slug,
+      title: post.frontmatter.title,
+      pubDate: post.frontmatter.publishDate,
+    })),
     // (optional) inject custom xml
     customData: `<language>en-us</language>`,
   });
