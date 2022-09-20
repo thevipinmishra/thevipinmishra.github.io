8319321a91932441bd567306c76ea0a01e65727b
Vipin Mishra <thevipinmishra@gmail.com>
2022-09-20 10:07:11 +0530

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +12,5 @@ export default defineConfig({
   markdown: {
     syntaxHighlight: "prism",
   },
-  integrations: [react(), sitemap(), mdx()],
+  integrations: [react(), sitemap()],
 });
